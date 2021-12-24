a02fc3a1b97aa451b8131ffd5c005e076008c31c
Ronnie-Leon76 <ronleon76@gmail.com>
2021-12-24 15:27:54 +0300

    Add user page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
-
+import React from "react";
+import AddUser from "./Components/Users/AddUser";
+import styled from "styled-components";
+import UsersList from './Components/Users/UsersList'
+const Section = styled.section`
+  width: 30rem;
+  max-width: 90%;
+  margin: 3rem auto;
+  padding: 2rem;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+`;
+const UsersListSection = styled.section`
+  width: 35rem;
+  max-width: 90%;
+  margin: 3rem auto;
+`;
+let users = (
+  <p style={{ textAlign: "center" }}>No users found. Maybe add one? </p>
+);
+const [usersList, setUsersList] = useState([
+  { name: "ELon Musk", age: '49', id: 'g1' },
+  { name: "Jeff Bezos", age: '50', id: 'g2' },
+]);
+if(usersList.length > 0){
+  users = (<UsersList items={usersList} />)
+}
 
 function App() {
   return (
     <div>
-
+      <Section>
+        <AddUser />
+      </Section>
+      <UsersListSection>
+        {users}
+      </UsersListSection>
     </div>
   );
 }
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,13 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import styled from "styled-components";
+import Button from "../UI/Button";
+
+const FormControl = styled.form`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+  &.invalid input {
+    border-color: red;
+    background: ${props => props.invalid ? 'ffd7d7' : 'transparent'};
+  }
+  &.invalid label {
+    color: ${props => props.invalid ? 'red' : 'black' };
+  }
+`;
 
 function AddUser() {
-    return (
-        <section>
-            <form>
-                
-            </form>
-        </section>
-    )
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [isValid, setIsValid] = useState(true)
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if((enteredName.trim().length === 0) && (enteredAge.trim().length === 0)){
+        setIsValid(false)
+    }
+    console.log(enteredName)
+    console.log(enteredAge)
+    
+  };
+  return (
+    <section>
+      <form onSubmit={formSubmitHandler}>
+        <FormControl invalid={!isValid}>
+          <label>Username</label>
+          <input
+            type="text"
+            value={enteredName}
+            onChange={(e) => setEnteredName(e.target.value)}
+          />
+          <label>Age</label>
+          <input
+            type="number"
+            value={enteredAge}
+            onChange={(e) => setEnteredAge(e.target.value)}
+          />
+        </FormControl>
+        <Button type="submit">Add User</Button>
+      </form>
+    </section>
+  );
 }
 
-export default AddUser
+export default AddUser;
